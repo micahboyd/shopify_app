78fbed401328d6c2f1e60ce57706a7aad92a4796
Micah <micah.boyd@protonmail.com>
2018-11-30 14:48:31 +1100

    Added env file, added omniauth fix from example app repo

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "e573bbeaa21e650d0c803cac9d81a245"
-  config.secret = "19fcba279a6ac5ebbdfcf0c09aff42aa"
+  config.api_key = ENV['SHOPIFY_API_KEY']
+  config.secret = ENV['SHOPIFY_SECRET']
   config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
